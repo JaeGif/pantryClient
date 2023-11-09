431afcb88b3a36d79ec43af2f6ea4475fd788bc9
Jae Gifford <jae.giff@outlook.com>
2023-11-09 09:18:29 -0800

    svg icons visible finally

--- App.tsx
+++ App.tsx
@@ -1,18 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
 import { StyleSheet, Text, View, Button, Pressable } from 'react-native';
+import Head from './components/general/Head';
 
 export default function App() {
-  const [counter, setCounter] = useState(0);
-  const incrementCount = () => {
-    setCounter(counter + 1);
-  };
   return (
     <View style={styles.container}>
-      <Text>This is the default Text component</Text>
-      <Pressable onPress={incrementCount} style={styles.button}>
-        <Text>This is a 'real' button with a random counter {counter}</Text>
-      </Pressable>
+      <Head />
       <StatusBar style='auto' />
     </View>
   );
---
+++ theme/theme.ts
@@ -0,0 +1,25 @@
+export const theme = {
+  color_scheme: {
+    light: {
+      black: '#131515',
+      white: '#fffafb',
+      primary_highlight: '#7de2d1',
+      secondary_highlight: '#339989',
+    },
+  },
+  card: {
+    container: {
+      // gets a shadow
+      boxShadow: 10,
+    },
+    content: {
+      icon: {},
+      text: {
+        title: {},
+        age: {},
+        location: {},
+        quantity: {},
+      },
+    },
+  },
+};
